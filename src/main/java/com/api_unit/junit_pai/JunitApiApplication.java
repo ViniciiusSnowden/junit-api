3bf14277dd072c2348cac40306dc809a72361c10
PAULO VINICIUS SILVA COSTA <paulo.costa@mv.com.br>
2024-10-08 13:49:01 -0300

    [Created] Projetc created with spring-init

---
+++ src/main/java/com/api_unit/junit_pai/JunitApiApplication.java
@@ -0,0 +1,13 @@
+package com.api_unit.junit_pai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_unit/junit_pai/JunitPaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_unit.junit_pai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitPaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
